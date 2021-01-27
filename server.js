5d56e38d044b2be63fc9bb61219be9116eb3dc82
laxmankadam123 <laxmankadam074@gmail.com>
2021-01-27 01:52:01 -0500

    commit

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.use(require('pino-http')({logger: pino}));
 
 app.get('/', (req, res) => {	
   // Use req.log (a `pino` instance) to log JSON:	
-  req.log.info({message: 'Hello from Asian Paints!..'});		
+  req.log.info({message: 'Hello from Asian Paints123!..'});		
   res.send('Hello from Asian Paints!..');	
 });	
 
